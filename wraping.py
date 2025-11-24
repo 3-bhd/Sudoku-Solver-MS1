f5a1ec3de74e448c2d4d7e07986f7dfa15317af8
Ahmed Ayman Abdelgalil <122588422+Ahmedayman55@users.noreply.github.com>
2025-11-24 21:52:48 +0200

    Bird's eye view transform

---
+++ wraping.py
@@ -0,0 +1,43 @@
+# warping.py
+import cv2
+import numpy as np
+
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]   # TL
+    rect[2] = pts[np.argmax(s)]   # BR
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]  # TR
+    rect[3] = pts[np.argmax(diff)]  # BL
+
+    return rect
+
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    widthA = np.linalg.norm(br - bl)
+    widthB = np.linalg.norm(tr - tl)
+    maxWidth = max(int(widthA), int(widthB))
+
+    heightA = np.linalg.norm(tr - br)
+    heightB = np.linalg.norm(tl - bl)
+    maxHeight = max(int(heightA), int(heightB))
+
+    side = max(maxWidth, maxHeight)
+
+    dst = np.array([
+        [0, 0],
+        [side - 1, 0],
+        [side - 1, side - 1],
+        [0, side - 1]
+    ], dtype="float32")
+
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (side, side))
+
+    return warped
